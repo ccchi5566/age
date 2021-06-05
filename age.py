fc892c2cf5584bdee4f96f20c338a996b1d03272
lucci lin <qqqqooo111@gmail.com>
2021-06-05 21:16:14 +0800

    age

---
+++ age.py
@@ -0,0 +1,16 @@
+#if 的雙層架構
+
+driving = input('有沒有開過車?')
+age = input('輸入年齡:' )
+age =int(age)
+if driving == '有':
+	if age >= 18:
+		print('通過測驗')
+	else:
+		print('偷開車')
+elif driving == '沒有':
+	if age >= 18:
+		print('可以去考駕照')
+	else:
+		print('快可以考了')
+
